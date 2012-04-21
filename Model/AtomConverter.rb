1e2003e7d47b798c19a7613e6ed8eea9bc1c4693
Daniel Machado Fernández <daniel.machado.fernandez@gmail.com>
2012-04-22 01:02:06 +0200

    Gemfile added

--- Model/AtomConverter.rb
+++
@@ -1,9 +0,0 @@
-#Class which converts an status into an Atom publication
-class AtomConverter
-
-	#Gets an status from the redis database an converts it into an standard Atom publication
-	def from_redis
-
-	end
-
-end
---
+++ Model/Twitter/Entity.rb
@@ -0,0 +1,5 @@
+class Entity
+	
+	attr_accessor :hashtags, :urls, :media, :user_mentions
+	
+end
